064f1ad3421eb57d1588f570dc5d936452474523
Дашноров дамдиндорж <23LP9512@G401-CC0213.local>
2024-03-18 12:46:21 +0800

    class

---
+++ src/components/SideMenu.tsx
@@ -0,0 +1,12 @@
+type Content = { name: string; price: number; isVerified: boolean };
+type Menu = { title: string; buttonText: string; content: Content[] };
+
+export default function SideMenu({ menu }: { menu: Menu }) {
+  return (
+    <div className="flex">
+      <div className="flex-col w-[30%] border bg-slate-500">
+        <button>{menu.buttonText}</button>
+      </div>
+    </div>
+  );
+}
